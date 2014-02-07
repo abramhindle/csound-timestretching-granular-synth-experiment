3c7e8d2f5052e023274609c5c81de0b58f66a773
Abram Hindle <abram.hindle@softwareprocess.es>
2014-02-06 22:15:23 -0700

    more grains!

---
+++ osctest.pl
@@ -0,0 +1,23 @@
+use Net::OpenSoundControl::Client;
+use strict;
+my $oschost = "127.0.0.1";
+my $oscport = 3666;
+{
+  my $client = undef;
+  sub client {
+    if (!$client) {
+      $client = Net::OpenSoundControl::Client->new(Host => $oschost, Port => $oscport ) or die "Could not start client: $@\n";
+
+    }
+    return $client;
+  }
+}
+
+client()->send(['/grain/gkPhase','f',rand()]);
+client()->send(['/grain/gkPhaseMix','f',rand()]);
+client()->send(['/grain/gkDur','f',0.1*rand()]);
+client()->send(['/grain/gkFreq','f',2*rand()*rand()*rand()]);
+client()->send(['/grain/gkFreqRand','f',rand()*0.1*rand()]);
+
+
+#push @elms,['/'.$command, @args];
